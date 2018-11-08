d8460d979e3956b2665d56c6d69514b9b7454b98
Ed Phelps <edphelps1@gmail.com>
2018-11-08 14:17:41 -0700

    search bar

--- src/App.js
+++ src/App.js
@@ -22,10 +22,14 @@ const Foot = () => (
 /* ********************************************
 *  SearchBar
 *********************************************** */
-const SearchBar = () => {
+const SearchBar = ({searchState}) => {
 
+  const onChange = (e) => {
+    console.log("search change");
+    e.preventDefault();
+  }
   const onSubmit = (e) => {
-    console.log("search submit");
+    console.log("onSubmit()");
     e.preventDefault();
   }
 
@@ -37,23 +41,40 @@ const SearchBar = () => {
 
           <div className="input-group input-group-sm">
 
-            {/*seach box*/}
-            {/*<div className="input-group-prepend">
-              <span className="input-group-text" id="search-btn">Search</span>
-            </div>*/}
-            <button type="submit">Search</button>
+            <button className="btn btn-sm btn-info" type="button">Reset</button>
             &nbsp;&nbsp;
-            <input id="search-text" type="text" className="form-control" />
+            {/*search box*/}
+            {/*<div className="input-group-prepend">
+              <span className="input-group-text" id="search-btn">Search:</span>
+            </div>*/}
+            <input id="search-text" type="text" className="form-control" placeholder="search"/>
 
-            {/*author or title radio buttons*/}
+            {/* author/title radio buttons */}
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <div className="form-check form-check-inline">
+            {/*<div className="form-check form-check-inline">
               <input className="form-check-input" type="radio" name="search-rbtn" id="author-rbtn" value="author-rbtn" />
               <label className="form-check-label" htmlFor="author-rbtn">author</label>
             </div>
             <div className="form-check form-check-inline">
               <input className="form-check-input" type="radio" name="search-rbtn" id="title-rbtn" value="title-rbtn" />
               <label className="form-check-label" htmlFor="title-rbtn">title</label>
+            </div>*/}
+
+            <div className="btn-group btn-group-toggle" data-toggle="buttons">
+              <label className="btn btn-outline-primary active">
+                <input type="radio" name="search-rbtns" id="author-rbtn" value="author" autoComplete="off" checked />
+                <label className="form-check-label" htmlFor="author-rbtn">author</label>
+              </label>
+              <label className="btn btn-outline-primary">
+                <input type="radio" name="search-rbtns" id="title-rbtn" value="author" autoComplete="off" />
+                <label className="form-check-label" htmlFor="title-rbtn">title</label>
+              </label>
+              {/*<label className="btn btn-secondary active">
+                <input type="radio" name="search-rbtns" id="author-rbtn" autoComplete="off" checked>author</input>
+              </label>
+              <label className="btn btn-secondary">
+                <input type="radio" name="search-rbtns" id="title-rbtn" autoComplete="off">title</input>
+              </label>*/}
             </div>
 
           </div> {/*input-group*/}
@@ -65,11 +86,20 @@ const SearchBar = () => {
 };
 
 class App extends Component {
+
+  state = {
+    search: {
+      text: "ed",
+      authorOrTitle: "author"
+    },
+  }
+
   render() {
+    const { search } = this.state;
     return (
       <div>
         <Nav />
-        <SearchBar />
+        <SearchBar searchState={search} />
         <p>content</p>
         <Foot />
       </div>
