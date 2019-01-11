4f2cbe92dd8124124f79381acb22bf78441367d7
Ed Phelps <edphelps1@gmail.com>
2019-01-11 11:11:10 -0700

    component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav'
 import Foot from './components/Foot'
+import SearchBar from './components/SearchBar'
 
 
 /* ********************************************
@@ -321,66 +322,6 @@ const Cart = ({ books, removeFromCartCB }) => {
   );
 };
 
-/* ********************************************
-*  SearchBar
-   Displays search bar at top and notifies parent of any change
-     searchState -- see App state
-     onChangeCB -- parent callback when content changes, passing
-       { text: "xx", authorOrTitle: "author" / "title" }
-}
-*********************************************** */
-const SearchBar = ({ searchCriteria, onChangeCB }) => {
-  const onChange = () => {
-    console.log('SearchBar::onChange()');
-    onChangeCB({
-      text: document.forms.searchForm.searchText.value.trim(),
-      authorOrTitle: document.forms.searchForm.searchRBtns.value,
-    });
-  };
-  const onSubmit = (e) => {
-    console.log('SearchBar::onSubmit() -- do nothing');
-    e.preventDefault();
-  };
-  const onReset = () => {
-    document.forms.searchForm.searchText.value = '';
-    document.getElementById('titleRBtn').checked = false;
-    document.getElementById('authorRBtn').checked = true;
-    onChange();
-  };
-
-  console.log('SearchBar::render()');
-  return (
-    <div>
-      <div id="search-bar" className="navbar bg-secondary text-light">
-        <form id="searchForm" onSubmit={onSubmit}>
-          <div className="input-group input-group-sm">
-
-            {/* search box */}
-            <button className="btn btn-sm btn-info" type="button" onClick={onReset}>Reset</button>
-            &nbsp;&nbsp;
-            <input id="searchText" onChange={onChange} type="text" className="form-control" value={searchCriteria.text} placeholder="search..." />
-
-            {/* author / title radio buttons */}
-            &nbsp;&nbsp;&nbsp;
-            <div className="form-check form-check-inline">
-              <label className="form-check-label" htmlFor="authorRBtn">
-                <input className="form-check-input" onChange={onChange} checked={searchCriteria.authorOrTitle === 'author'} type="radio" name="searchRBtns" id="authorRBtn" value="author" />
-                author
-              </label>
-            </div>
-            <div className="form-check form-check-inline">
-              <label className="form-check-label" htmlFor="titleRBtn">
-                <input className="form-check-input" onChange={onChange} checked={searchCriteria.authorOrTitle === 'title'} type="radio" name="searchRBtns" id="titleRBtn" value="title" />
-                title
-              </label>
-            </div>
-
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-};
 
 /* *************************************************
 *
